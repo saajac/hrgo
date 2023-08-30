76ac21536439109967c6ee24dd1e752100303e63
ben Khaireh <62518373+benkhaireh@users.noreply.github.com>
2023-08-30 16:01:33 +0300

    Petit modification de letat de salaire

--- app/Http/Controllers/PaySlipController.php
+++ app/Http/Controllers/PaySlipController.php
@@ -127,22 +127,24 @@ class PaySlipController extends Controller
                         }
                     }
 
-                    /* $abatt      = SaturationDeduction::where('employee_id', '=', $employee->id)->where('deduction_option', '=', 2)->get();
+                    $abatt      = SaturationDeduction::where('employee_id', '=', $employee->id)->where('deduction_option', '=', 2)->where('type', '=', 'percentage')->get();
+                    count($abatt) <= 0 ? $abatt = 0 : $abatt = $abatt[0]->amount;
 
-                    $cnr      = SaturationDeduction::where('employee_id', '=', $employee->id)->where('deduction_option', '=', 1)->get(); */
+                    $cnr      = SaturationDeduction::where('employee_id', '=', $employee->id)->where('deduction_option', '=', 1)->where('type', '=', 'percentage')->get();
+                    count($cnr) <= 0 ? $cnr = 0 : $cnr = $cnr[0]->amount;
 
                     SaturationDeduction::where('employee_id', '=', $employee->id)->where('deduction_option', '=', 2)->update([
-                        'amount' => (int) bcmul((($employee->salary + $total_allowance) / 100), 5),
+                        'amount' => (int) bcmul((($employee->salary + $total_allowance) / 100), $abatt),
                         'type' => 'fixed'
                     ]);
 
                     SaturationDeduction::where('employee_id', '=', $employee->id)->where('deduction_option', '=', 5)->update([
-                        'amount' => (int) bcmul((($employee->salary + $total_allowance - ((int) bcmul((($employee->salary + $total_allowance) / 100), 5))) / 100), 2),
+                        'amount' => (int) bcmul((($employee->salary + $total_allowance - ((int) bcmul((($employee->salary + $total_allowance) / 100), $abatt))) / 100), 2),
                         'type' => 'fixed'
                     ]);
 
                     SaturationDeduction::where('employee_id', '=', $employee->id)->where('deduction_option', '=', 1)->update([
-                        'amount' => (int) bcmul(($employee->salary / 100), 7),
+                        'amount' => (int) bcmul(($employee->salary / 100), $cnr),
                         'type' => 'fixed'
                     ]);
 
